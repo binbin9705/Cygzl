5f26bf973e0cfedb093dff25bd9d42d6fb5da193
glabla <binbin9705@163.com>
2021-12-06 15:59:14 +0800

    调试

--- case/test_case1.py
+++ case/test_case1.py
@@ -1,12 +1,9 @@
 import unittest
 import time
 from selenium import webdriver
-
 from PublicMethod import WebGetDemo
 import warnings
-
 from selenium.webdriver.common.by import By
-
 
 class Test1(unittest.TestCase):
     '''页面跳转'''
@@ -28,7 +25,7 @@ class Test1(unittest.TestCase):
     def tearDown(self):
         pass
 
-    # @unittest.skip('调试')
+    @unittest.skip('调试')
     def test_01(self):
         '''首页跳转CRL指数页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -46,7 +43,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('CRI指数 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_02(self):
         '''首页跳转企业总量页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -64,7 +61,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('企业总量 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_03(self):
         '''首页跳转行业分析页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -82,7 +79,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('行业分析 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_04(self):
         '''首页跳转结构分析页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -100,7 +97,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('结构分析 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_05(self):
         '''首页跳转重点企业页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -118,7 +115,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('重点企业 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_06(self):
         '''首页跳转总体监测页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -136,7 +133,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('总体监测 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_07(self):
         '''首页跳转产业分析页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -154,7 +151,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('产业分析 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_08(self):
         '''首页跳转产业地图页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -170,7 +167,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('产业地图 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_09(self):
         '''首页跳转创新主体页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -188,7 +185,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('创新主体 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_10(self):
         '''首页跳转创新成果页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -206,7 +203,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('创新成果 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_11(self):
         '''首页跳转产业链图谱页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -222,7 +219,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('产业链图谱 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_12(self):
         '''首页跳转招商图谱页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -240,7 +237,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('招商图谱 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_13(self):
         '''首页跳转资本流动页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -258,7 +255,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('资本流动 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_14(self):
         '''首页跳转开发发展-结构分析页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -276,7 +273,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('结构分析 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_15(self):
         '''首页跳转地域分析页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -294,7 +291,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('地域分析 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_16(self):
         '''首页跳转企业分析页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -312,7 +309,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('企业分析 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    # @unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_17(self):
         '''首页跳转总体概览页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
@@ -330,7 +327,7 @@ class Test1(unittest.TestCase):
         self.assertEqual('总体概览 - 产业高质量发展平台', self.driver.title, '用例执行错误')
         WebGetDemo.Runmian(self.driver).quit()
 
-    #@unittest.skip('跳过')
+    @unittest.skip('跳过')
     def test_18(self):
         '''首页跳转结构分析页面'''
         WebGetDemo.Runmian(self.driver).login('ihqd-test', 'ihqd-test@6688')
