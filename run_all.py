57126eedfe496c92e57a2530168104fdbc44cc05
glabla <binbin9705@163.com>
2022-01-04 17:43:58 +0800

    添加用例失败后重新执行取最后一次结果（共三次）

--- run_all.py
+++ run_all.py
@@ -5,15 +5,16 @@ import HTMLTestRunner_cn
 from case import test_case1
 if __name__ == '__main__':
     #创建一个测试套件
-    dir='case'
+    # dir='case'
     dircase='test*.py'
+    dircase = 'test_case5.py'
     # dircase = 'test_case3.py'
     suite=unittest.defaultTestLoader.discover(dir,dircase)
     #创建生成测试报告方法
     # now=time.strftime('%Y-%m-%d %H_%M_%S')
     path='plugins/测试报告.html'
     pathopen=open(path,'wb')
-    runner=HTMLTestRunner_cn.HTMLTestRunner(stream=pathopen,description=u'用例执行情况',title='产业高质量发展平台测试报告',verbosity=2)
+    runner=HTMLTestRunner_cn.HTMLTestRunner(stream=pathopen,description=u'用例执行情况',title='产业高质量发展平台测试报告',verbosity=2,retry=2, save_last_try=True)
     runner.run(suite)
 
 
