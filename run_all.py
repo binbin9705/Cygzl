6d6553f3b39f5b7704a7b007f81e4210b28e9e92
glabla <binbin9705@163.com>
2022-01-10 11:01:54 +0800

    删除多余封装方法

--- run_all.py
+++ run_all.py
@@ -6,14 +6,14 @@ from case import test_case1
 if __name__ == '__main__':
     #创建一个测试套件
     dir='case'
-    # dircase='test*.py'
-    dircase = 'test_case4.py'
+    dircase='test*.py'
+    # dircase = 'test_case4.py'
     suite=unittest.defaultTestLoader.discover(dir,dircase)
     #创建生成测试报告方法
     # now=time.strftime('%Y-%m-%d %H_%M_%S')
     path='plugins/测试报告.html'
     pathopen=open(path,'wb')
-    runner=HTMLTestRunner_cn.HTMLTestRunner(stream=pathopen,description=u'用例执行情况',title='产业高质量发展平台测试报告',verbosity=2,retry=2, save_last_try=True)
+    runner=HTMLTestRunner_cn.HTMLTestRunner(stream=pathopen,description=u'用例执行情况',title='产业高质量发展平台测试报告',verbosity=2,retry=1, save_last_try=True)
     runner.run(suite)
 
 
